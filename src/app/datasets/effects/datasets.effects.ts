57ed9c4ce644d1e6f162a8938faeeca21aeb95a7
zero3smart <fivewisdomzero@gmail.com>
2019-05-15 03:10:10 -0300

    [General] Code Cleanup

--- src/app/datasets/effects/datasets.effects.ts
+++ src/app/datasets/effects/datasets.effects.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
 
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
 import { DatasetsService } from '../datasets.service';
-import { DatasetsActionsType, GetAnnotations, GetAnnotationsSuccess } from '../actions/actions';
+import { DatasetsActionsType, GetAnnotations, GetAnnotationsSuccess, GetAnnotationsError } from '../actions/actions';
 
 @Injectable()
 export class DatasetsEffects {
@@ -12,8 +15,9 @@ export class DatasetsEffects {
 
     @Effect() getAnnotations$ = this.actions$.ofType(DatasetsActionsType.GET_ANNOTATIONS)
         .map((action: GetAnnotations) => action.payload)
-        .mergeMap(dataset => {
+        .mergeMap(({dataset, progress}) => {
             return this.datasetsService.getAnnotations(dataset)
-                .map(annotations => new GetAnnotationsSuccess({dataset, annotations}));
+                .map(annotations => new GetAnnotationsSuccess({dataset, annotations}))
+                .catch(error => Observable.of(new GetAnnotationsError({ dataset, error })));
         });
 }
