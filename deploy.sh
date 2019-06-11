953eca66ec34d84f25b58e1d3d793afba4b70f3b
zero3smart <fivewisdomzero@gmail.com>
2019-06-10 21:01:39 -0300

    deploy script modify git path

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ git submodule add -f https://$site_user:$site_pass@strayossite.scm.azurewebsites
 rm -rf build/*
 ng build
 cp -r dist/* build/
+cd build/
 git config --global user.email "sahil@strayos.com"
 git config --global user.name "Sahil Kataria"
 git add -A .
