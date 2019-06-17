8b0288e4dcddc43ed56358ffaee745cd56f825e4
zero3smart <fivewisdomzero@gmail.com>
2019-06-17 12:10:56 -0300

    [General] Updated pipelines deployment

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ npm install -g @angular/cli
 yarn install
 git submodule add -f https://$site_user:$site_pass@strayossite.scm.azurewebsites.net/strayossite.git build
 rm -rf build/*
-ng build
+ng build --env=prod
 cp -r dist/* build/
 cd build/
 git config --global user.email "sahil@strayos.com"
 git config --global user.name "Sahil Kataria"
 git add -A .
 git commit -a -m "Deployment"
-git push origin master
+git push -f origin master
--- src/app/util/getApiUrl.ts
+++ src/app/util/getApiUrl.ts
@@ -3,6 +3,7 @@ import { environment as e } from '../../environments/environment';
 export const BACKEND_URL = `${e.http}://${e.be_host}:${e.be_port}`;
 export const GRAPHQL_URL = `${BACKEND_URL}/graphql`;
 export const API_URL = `${BACKEND_URL}/${e.api_prefix}`;
+console.log('urls', BACKEND_URL, GRAPHQL_URL, API_URL);
 
 export const SITE_URL = 'sites';
 export const DATASET_URL = 'datasets';
