9001ee0f78cbb9a7150d08edc520592c17573e11
zero3smart <fivewisdomzero@gmail.com>
2019-06-26 19:38:08 -0300

    [General] Line Measurements Work

--- src/app/models/annotationToolType.ts
+++ src/app/models/annotationToolType.ts
@@ -1,1 +1,12 @@
-export type AnnotationToolType = 'polygon' | 'horizontalLength' | 'selection' | 'relativeHeight';
+export type AnnotationToolType = 'Polygon' | 'LineString' | 'Selection' | 'Height';
+
+
+export const ToolToType = {
+    LineString: 'LineString',
+    Polygon: 'Polygon',
+};
+
+export const ToolToThumbnail = {
+    LineString: 'horizontalLength',
+    Polygon: 'polygon',
+};
