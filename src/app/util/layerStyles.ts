ee0469ad15f04aaa019aa5e4a60694a7de25f15b
zero3smart <fivewisdomzero@gmail.com>
2019-07-03 11:36:22 -0300

    [General] Basic Shotplan Functionality

--- src/app/util/layerStyles.ts
+++ src/app/util/layerStyles.ts
@@ -1,5 +1,10 @@
 import * as ol from 'openlayers';
 
+
+const nullFill = new ol.style.Fill();
+const nullStroke = new ol.style.Stroke();
+const nullText = new ol.style.Text();
+
 export const siteMarkerStyle = new ol.style.Style({
     stroke: new ol.style.Stroke({
         color: 'rgba(200,200,200,0.5)',
@@ -33,6 +38,26 @@ export const countryStyle = new ol.style.Style({
     })
 });
 
+export const annotationInteractionStyle = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: 'rgba(255, 255, 255, 0.2)'
+    }),
+    stroke: new ol.style.Stroke({
+        color: 'rgba(0, 0, 0, 0.5)',
+        lineDash: [10, 10],
+        width: 2
+    }),
+    image: new ol.style.Circle({
+        radius: 5,
+        stroke: new ol.style.Stroke({
+            color: 'rgba(0, 0, 0, 0.7)'
+        }),
+        fill: new ol.style.Fill({
+            color: 'rgba(255, 255, 255, 0.2)'
+        })
+    })
+});
+
 export function annotationStyle(feature: ol.Feature, resolution: number) {
     const geom = feature.getGeometry();
     switch (geom.getType()) {
@@ -56,6 +81,66 @@ export function annotationStyle(feature: ol.Feature, resolution: number) {
     }
 }
 
+export function shotplanStyle(feature: ol.Feature, resolution: number) {
+    const geometry = feature.getGeometry();
+    return [
+        new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: 'aquamarine',
+            }),
+            geometry: function (feature) {
+                return (geometry as ol.geom.GeometryCollection).getGeometries().find(g => g.getType() === 'LineString');
+            }
+        }),
+        new ol.style.Style({
+            image: new ol.style.Circle({
+                radius: 5,
+                fill: new ol.style.Fill({
+                    color: 'white',
+                }),
+                stroke: new ol.style.Stroke({
+                    color: 'aquamarine'
+                })
+            }),
+            geometry: function(feature) {
+                const coordinates = [
+                    ...(geometry as ol.geom.GeometryCollection).getGeometries()
+                        .filter(g => g.getType() === 'MultiPoint')
+                        .map((g: ol.geom.MultiPoint) => g.getFirstCoordinate())
+                ];
+                return new ol.geom.MultiPoint(coordinates);
+            }
+        })]
+
+    // if (geometry.getType() === 'GeometryCollection') {
+    //     console.log('here in geometry collection');
+    //     return withStyles(...(geometry as ol.geom.GeometryCollection).getGeometries().map((geom) => shotplanStyle(new ol.Feature({ geometry: geom }), resolution)))(feature, resolution);
+
+    // } else if (geometry.getType() === 'LineString') {
+    //     console.log('got this geometry');
+    //     return new ol.style.Style({
+    //         stroke: new ol.style.Stroke({
+    //             color: 'aquamarine',
+    //             width: 3
+    //         }),
+    //         fill: new ol.style.Fill({
+    //             color: 'aquamarine'
+    //         })
+    //     });
+    // }else {
+
+    //     return new ol.style.Style({
+    //         stroke: new ol.style.Stroke({
+    //             color: 'white',
+    //             width: 3
+    //         }),
+    //         fill: new ol.style.Fill({
+    //             color: 'aquamarine'
+    //         })
+    //     });
+    // }
+}
+
 export function withStyles(...styles: Array<ol.style.Style | ol.style.Style[] | ol.StyleFunction>): ol.StyleFunction {
     return (feature: ol.Feature, resolution: number) => {
         const toReturn = [];
@@ -75,31 +160,32 @@ export function withStyles(...styles: Array<ol.style.Style | ol.style.Style[] | 
                 toReturn.push(...style);
             }
         });
+        console.log('toReturn', toReturn);
         return toReturn;
+        // return toReturn.reduce(styleReducer, new ol.style.Style({}));
     };
-
-    function styleReducer(acc: ol.style.Style, current: ol.style.Style) {
-        if (acc.getFill() && current.getFill()) {
-            acc.setFill(current.getFill());
-        }
-        if (acc.getStroke() && current.getStroke()) {
-            const p = acc.getStroke();
-            const c = current.getStroke();
-            acc.setStroke(new ol.style.Stroke({
-                color: c.getColor() || p.getColor(),
-                width: c.getWidth() || p.getWidth(),
-                lineCap: c.getLineCap() || p.getLineCap(),
-                lineJoin: c.getLineJoin() || p.getLineJoin(),
-                miterLimit: c.getMiterLimit() || p.getMiterLimit(),
-            }));
-            acc.setStroke(current.getStroke());
-        }
-        if (acc.getText() && current.getText()) {
-            acc.setText(current.getText());
-        }
-        if (acc.getImage() && current.getImage()) {
-            acc.setImage(current.getImage());
-        }
-        return acc;
-    }
+    // Please fill in as you go along
+    // function styleReducer(acc: ol.style.Style, current: ol.style.Style) {
+    //     console.log('reducing', acc, current)
+    //     if (current.getFill()) {
+    //         const p = acc.getFill() || nullFill;
+    //         const c = current.getFill();
+    //         acc.setFill(new ol.style.Fill({
+    //             color: c.getColor() || p.getColor(),
+    //         }))
+    //     }
+    //     if (current.getStroke()) {
+    //         const p = acc.getStroke() || nullStroke;
+    //         const c = current.getStroke();
+    //         acc.setStroke(new ol.style.Stroke({
+    //             color: c.getColor() || p.getColor(),
+    //             width: c.getWidth() || p.getWidth(),
+    //             lineCap: c.getLineCap() || p.getLineCap(),
+    //             lineDash: c.getLineDash() || p.getLineDash(),
+    //             lineJoin: c.getLineJoin() || p.getLineJoin(),
+    //             miterLimit: c.getMiterLimit() || p.getMiterLimit(),
+    //         }))
+    //     }
+    //     return acc;
+    // }
 }
